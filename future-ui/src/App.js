5aa592fc453171c6decf521be49345f6d7efb77a
vezzita <minyoung.vezzita@gmail.com>
2018-03-19 22:24:28 +0900

    tag 입력 기본 이벤트 구현

--- future-ui/src/App.js
+++ future-ui/src/App.js
@@ -2,12 +2,61 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+      currentKeyword: "",
+    }
+  }
+
+  handleOnChange = e => {
+    this.setState({ currentKeyword: e.target.value })
+  }
+
+  handleKeyPress = e => {
+    const { tags, currentKeyword } = this.state;
+
+    if(e.key === " " || e.key === "Enter") {
+      this.setState({
+        tags: [
+          ...tags,
+          { tag: currentKeyword }
+        ],
+        currentKeyword: ""
+      })
+    }
+  }
+
   render() {
+    const {
+      tags,
+      currentKeyword,
+    } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">FUTURE UI</h1>
         </header>
+        <section>
+          <input
+            type="text"
+            onKeyPress={this.handleKeyPress}
+            onChange={this.handleOnChange}
+            value={currentKeyword}
+          />
+        </section>
+        <section>
+          {tags.map((item, index) => (
+            <div
+              className="tag-box"
+              key={index}
+            >
+              {item.tag}
+            </div>
+          ))}
+        </section>
       </div>
     );
   }
